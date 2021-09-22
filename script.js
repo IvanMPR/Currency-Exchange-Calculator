f787dc6b4f3dc5294f47a13b6cebd5bb39b96b6e
Ivan MPR <ivsanmas@gmail.com>
2021-09-22 22:02:14 +0200

    comments added and finalization

--- script.js
+++ script.js
@@ -19,46 +19,49 @@ async function getData(curr) {
     if (response.result === 'error') {
       throw new Error('Unknown Currency Code');
     }
-    console.log(response);
+
     // Copy response rates to data object
     data.rates = response.rates;
-    console.log(data.rates);
+
     const pair = outputList.value;
     const fixed = response.rates[curr]; // Default selected currency value, always 1
-    baseInput.value = baseInput.value || fixed;
+    baseInput.value = baseInput.value || fixed; // If empty, input will be 1, otherwise input value
     output.value = (baseInput.value * fixed * response.rates[pair]).toFixed(2);
     displayRate(curr, data, pair);
   } catch (err) {
-    console.error(err);
     errorMessage(outputParagraph, err);
   }
 }
-
+// Calc values between inputs without calling the API
 function calcRate(baseInp, obj, pair) {
   const result = +baseInp.value * obj.rates[pair];
   return (output.value = result.toFixed(2));
 }
-
+// Display conversion rate for current pair
 function displayRate(currency, obj, pair) {
   outputParagraph.textContent = `1 ${currency} = ${obj.rates[pair]} ${pair}`;
 }
-
+// Calculate and display new value when output list change value(currency)
 function onOutputListChange(obj) {
   const newCurrency = outputList.value;
   output.value = (+baseInput.value * obj.rates[newCurrency]).toFixed(2);
   displayRate(baseList.value, data, outputList.value);
 }
+// Swap button function.
 function swapPair() {
+  // swap currencies
   const base = baseList.value;
   const pair = outputList.value;
   baseList.value = pair;
   outputList.value = base;
-  console.log(output.value, baseInput.value);
+  // swap input values
   const baseAmount = baseInput.value;
   const outputAmount = output.value;
   baseInput.value = outputAmount;
   output.value = baseAmount;
+  // call API with new base currency
   getData(baseList.value);
+  // display new conversion rate between base and pair currencies
   displayRate(baseList.value, data, outputList.value);
 }
 // Listeners
@@ -69,26 +72,27 @@ window.addEventListener('load', function () {
   currencyList.forEach(el => (el.label = ''));
   // API Call
   getData('USD');
+  // prevent Firefox not displaying usd eur values after page refresh
   baseList.value = 'USD';
   outputList.value = 'EUR';
 });
-
+// Call API on change for baseList
 baseList.addEventListener('change', function () {
   getData(baseList.value);
 });
-
+// Calculate and display amount between currencies
 baseInput.addEventListener('change', function () {
   calcRate(baseInput, data, outputList.value);
 });
-
+// Calc and display value when different currency is picked from output list
 outputList.addEventListener('change', function () {
   onOutputListChange(data);
 });
-
+// Reversed process, calc input value on output value change
 output.addEventListener('change', calcRateReversed);
-
+// Swap values
 swapBtn.addEventListener('click', swapPair);
-
+// Parse conversion rate from output paragraph(1 USD = 0.95 EUR.  Get 0.95 from string )
 function calcRateReversed() {
   const parsedRate = outputParagraph.textContent
     .split('=')[1]
@@ -98,10 +102,12 @@ function calcRateReversed() {
     .map(el => +el);
   return (baseInput.value = (+output.value / parsedRate[0]).toFixed(2));
 }
+// Render message on error
 function errorMessage(el, error) {
   el.innerHTML = '';
   el.textContent = `${error.message}! Try again!`;
 }
+// Reject if request took too long
 function timeout(sec) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
